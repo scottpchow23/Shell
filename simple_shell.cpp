6a104acfdb77f772a750142576a88e1dd4ecbbc4
Scott Chow <tehchowster@gmail.com>
2019-04-06 12:16:27 -0700

    Example of running another program from the first one.

--- simple_shell.cpp
+++ simple_shell.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
+#include <unistd.h>
 #define MAX_LINE_SIZE 512
+#define MAX_TOKEN_SIZE 32
+
+void parseInputLine(std::string inputLine);
 
 int main(int argc, char *argv[]) {
   // bool output = true;
@@ -14,6 +18,10 @@ int main(int argc, char *argv[]) {
       break;
     } else {
       std::cin >> inputLine;
+      
+      parseInputLine(inputLine);
+      
+
     }
     
     std::cout << "Echo:" << inputLine << std::endl;
@@ -21,4 +29,26 @@ int main(int argc, char *argv[]) {
   }
 
   return 0;
+}
+
+void parseInputLine(std::string inputLine) {
+
+  if (inputLine == "ls") {
+    pid_t childPID = fork();
+    int status;
+
+    if (childPID == 0) {
+      // child process
+      char * ls_args[2];
+      ls_args[0] = ".";
+      ls_args[1] = 0;
+      
+      execve("/bin/ls", ls_args, 0);
+    } else {
+      // parent process
+      waitpid(childPID, &status, 0);
+      std::cout << "Child process " << childPID <<" returned with status code: " << status << std::endl;
+    }
+  }
+
 }
